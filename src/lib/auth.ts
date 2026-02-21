35edcee81cd3133706487e1e8f465c471bcf235d
Debajit91 <debajitroy544@gmail.com>
2026-02-21 15:18:06 +0600

    google sign up option added using better auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -19,6 +19,14 @@ export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql", 
     }),
+
+    socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID as string, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+        }, 
+    },
+    
     user:{
       additionalFields:{
         role:{
