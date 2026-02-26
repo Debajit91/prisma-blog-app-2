6179b1912b5ec17e4dd7d15026d74e0def0e3d35
Debajit91 <debajitroy544@gmail.com>
2026-02-26 22:19:50 +0600

    implemented search using OR and HAS

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,30 +1,47 @@
 import { Post } from "../../../generated/prisma/client";
 import { prisma } from "../../lib/prisma";
 
-const createPost = async(data: Omit<Post, "id" | "createdAt" | "updatedAt" | "authorId">, userId:string)=>{
-    const result = await prisma.post.create({
-        data:{
-            ...data,
-            authorId: userId
-        }
-    })
-    return result;
-}
+const createPost = async (
+  data: Omit<Post, "id" | "createdAt" | "updatedAt" | "authorId">,
+  userId: string,
+) => {
+  const result = await prisma.post.create({
+    data: {
+      ...data,
+      authorId: userId,
+    },
+  });
+  return result;
+};
 
-
-const getAllPost = async(payload: {search?: string | undefined})=>{
-    const allPost = await prisma.post.findMany({
-        where:{
-            title:{
-                contains: payload.search as string,
-                mode: "insensitive"
+const getAllPost = async (payload: { search?: string | undefined }) => {
+  const allPost = await prisma.post.findMany({
+    where: {
+      OR: [
+        {
+          title: {
+            contains: payload.search as string,
+            mode: "insensitive",
+          },
+        },
+        {
+          content: {
+            contains: payload.search as string,
+            mode: "insensitive",
+          },
+        },
+        {
+            tags:{
+                has: payload.search as string,
             }
-        }}
-    );
-    return allPost;
-}
+        }
+      ],
+    },
+  });
+  return allPost;
+};
 
-export const postServices ={
-    createPost,
-    getAllPost
-}
+export const postServices = {
+  createPost,
+  getAllPost,
+};
